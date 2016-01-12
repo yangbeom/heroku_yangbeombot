13f578249c6c7faf12ab6887d347f6bbdd49435a
yangbeom <yangbeomsu@gmail.com>
2016-01-12 21:50:09 +0900

    init commit

--- herokubot.py
+++ herokubot.py
@@ -32,7 +32,6 @@ def hello():
 @app.route('/'+os.environ['TELEGRAM_TOKEN']+'/',methods=["POST","GET"])
 def token():
     if request.method == "POST":
-        return "bye"
         print(request.content_type)
         print("content_type")
         print(request.get_json())
