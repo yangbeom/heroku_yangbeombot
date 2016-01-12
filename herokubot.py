ce9d9b1a211316c61259f6ed5551d2b3753369cf
yangbeom <yangbeomsu@gmail.com>
2016-01-13 01:04:00 +0900

    fix pep8

--- herokubot.py
+++ herokubot.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 import json
 import requests
 import re
@@ -10,48 +10,42 @@ howtouse = """사용법
 /poster 영화제목
 영화 포스터를 전송합니다."""
 
-def naver_movie(q,data):
-    url = "http://auto.movie.naver.com/ac"
-    params = {"q_enc":"UTF-8","st":"1","r_lt":"1","n_ext" : "1","t_koreng":"1","r_format":"json","r_enc":"UTF-8",
-            "r_unicode":"0","r_escape":"1","q":q}
 
-    r = requests.get(url,params=params)
+def naver_movie(q, data):
+    url = "http://auto.movie.naver.com/ac"
+    params = {"q_enc": "UTF-8", "st": "1", "r_lt": "1", "n_ext": "1", "t_koreng": "1", "r_format": "json",
+              "r_enc": "UTF-8", "r_unicode": "0", "r_escape": "1", "q": q}
+
+    r = requests.get(url, params=params)
     contents = json.loads(r.content.decode("utf-8"))
     mv_poster_url = contents['items'][0][0][3][0]
-    print(mv_poster_url)
-    r= requests.get(mv_poster_url)
-    with open("out.jpg","wb+") as f:
+    r = requests.get(mv_poster_url)
+    with open("out.jpg", "wb+") as f:
         f.write(r.content)
         f.seek(0)
-        INFO = {"chat_id": data['message']['from']['id']}
-        files= {"photo" : f}
-        r = requests.post("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendPhoto",files=files,data=INFO,stream=True)
+        info = {"chat_id": data['message']['from']['id']}
+        files = {"photo": f}
+        requests.post("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendPhoto", files=files,
+                      data=info, stream=True)
 
 
 @app.route('/')
 def hello():
     return "hello"
 
-@app.route('/'+os.environ['TELEGRAM_TOKEN']+'/',methods=["POST","GET"])
+
+@app.route('/'+os.environ['TELEGRAM_TOKEN']+'/', methods=["POST", "GET"])
 def token():
     if request.method == "POST":
-        print(request.content_type)
-        print("content_type")
-        print(request.get_json())
-        print("request.get_json()")
         getjson = request.get_json()
-        print(getjson)
-        print("before re")
         try:
-            REresult = re.search(rpattern,getjson['message']['text'])
-            print(REresult.group("command"))
-            if REresult.group("command") == "poster":
-                print(REresult.group('q'))
-                naver_movie(REresult.group('q'),getjson)
+            reresult = re.search(rpattern, getjson['message']['text'])
+            if reresult.group("command") == "poster":
+                naver_movie(reresult.group('q'), getjson)
             else:
                 return "notthing your command"
         except:
-            INFO = {"chat_id": getjson['message']['from']['id'],"text":howtouse}
-            requests.get("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendMessage",params=INFO)
+            info = {"chat_id": getjson['message']['from']['id'], "text": howtouse}
+            requests.get("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendMessage", params=info)
 
     return "Success"
