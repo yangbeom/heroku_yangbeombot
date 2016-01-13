ddc5944f803caa637729c31cd55a5ed14e8df3d6
yangbeom <yangbeomsu@gmail.com>
2016-01-13 12:38:55 +0900

    fix pep8

--- herokubot.py
+++ herokubot.py
@@ -5,13 +5,17 @@ import re
 import os
 
 app = Flask(__name__)
+rcommand = r'^\/'
 rpattern = r'^\/(?P<command>.*) (?P<q>.*)'
-howtouse = """사용법
+
+def how_to_use(jsondata):
+    howtouse = """사용법
 /poster 영화제목
 영화 포스터를 전송합니다."""
+    info = {"chat_id": json['message']['from']['id'], "text": howtouse}
+    requests.get("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendMessage", params=info)
 
-
-def naver_movie(q, data):
+def naver_movie(q, jsondata):
     url = "http://auto.movie.naver.com/ac"
     params = {"q_enc": "UTF-8", "st": "1", "r_lt": "1", "n_ext": "1", "t_koreng": "1", "r_format": "json",
               "r_enc": "UTF-8", "r_unicode": "0", "r_escape": "1", "q": q}
@@ -23,7 +27,7 @@ def naver_movie(q, data):
     with open("out.jpg", "wb+") as f:
         f.write(r.content)
         f.seek(0)
-        info = {"chat_id": data['message']['from']['id']}
+        info = {"chat_id": jsondata['message']['from']['id']}
         files = {"photo": f}
         requests.post("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendPhoto", files=files,
                       data=info, stream=True)
@@ -39,13 +43,15 @@ def token():
     if request.method == "POST":
         getjson = request.get_json()
         try:
-            reresult = re.search(rpattern, getjson['message']['text'])
-            if reresult.group("command") == "poster":
-                naver_movie(reresult.group('q'), getjson)
-            else:
-                return "notthing your command"
+            if re.search(rcommand, getjson['message']['text']):
+                reresult = re.search(rpattern, getjson['message']['text'])
+                if reresult.group("command") == "poster":
+                    naver_movie(reresult.group('q'), getjson)
+                elif reresult.group("command") == "start":
+                    how_to_use(getjson)
+                else:
+                    return "notthing your command"
         except:
-            info = {"chat_id": getjson['message']['from']['id'], "text": howtouse}
-            requests.get("https://api.telegram.org/bot"+os.environ['TELEGRAM_TOKEN']+"/sendMessage", params=info)
+            how_to_use(getjson)
 
     return "Success"
